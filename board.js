289eafd64ca68cab32e14234267ce0a22c14c584
Craig Young <craigjyoung56@gmail.com>
2023-12-16 13:53:10 -0500

    initial commit

---
+++ board.js
@@ -0,0 +1,123 @@
+var board = {
+    "members": [
+        {
+            "title": "President",
+            "name": "Bruce Smentek",
+            "phone": "(805) 368-0414",
+            "email": "smetnek31@yahoo.com",
+            "img": "./images/gallery/Bruce.jpeg"
+        },
+        {
+            "title": "Vice President",
+            "name": "Neal Martin",
+            "phone": "(805) 606-6020",
+            "email": "neal3307@gmail.com",
+            "img": ""
+        },
+        {
+            "title": "Secretary",
+            "name": "Don Ludwig",
+            "phone": "(818) 846-8943",
+            "email": "ludwig@usc.edu",
+            "img": "./images/gallery/don_ludwig.jpg"
+        },
+        {
+            "title": "Treasurer/Greens",
+            "name": "Steve Sutliff",
+            "phone": "(818) 261-4137",
+            "email": "stevesutliff@sbcglobal.net",
+            "img": ""
+        },
+        {
+            "title": "Tournaments",
+            "name": "Peter Hendrickson",
+            "phone": "(818) 846-1812",
+            "email": "l8sleeper@earthlink.net",
+            "img": "./images/gallery/peter_hendrickson.jpeg"
+        },
+        {
+            "title": "Rules",
+            "name": "Mike Samaniego",
+            "phone": "(626) 281--3394",
+            "email": "sammy04@hotmail.com",
+            "img": ""
+        },
+        {
+            "title": "Handicap",
+            "name": "Bob Ranney",
+            "phone": "(818) 823-3903",
+            "email": "bubbajoebob@sbcglobal.net",
+            "img": ""
+        },
+        {
+            "title": "SGCA",
+            "name": "Mike Davis",
+            "phone": "(818) 955-9447",
+            "email": "mwdbfd44@gmail.com",
+            "img": ""
+        },
+        {
+            "title": "News Info",
+            "name": "John Shannon",
+            "phone": "(818) 353-5378",
+            "email": "johnshannon422@gmail.com",
+            "img": "./images/gallery/john_shannon.jpg"
+        },
+        {
+            "title": "Membership",
+            "name": "Peter Hendrickson",
+            "phone": "(818) 846-1812",
+            "email": "l8sleeper@earthlink.net",
+            "img": "./images/gallery/peter_hendrickson.jpeg"
+        },
+        {
+            "title": "Inter-Club Play",
+            "name": "Neal Martin",
+            "phone": "(818) 606-6020",
+            "email": "neal3307@gmail.com",
+            "img": ""
+        },
+        {
+            "title": "H.S. Golf Liason",
+            "name": "Mike Krose",
+            "phone": "(323) 821-1421",
+            "email": "mikekrose@roarrunner.com",
+            "img": "./images/gallery/mike_krose.jpg"
+        },
+        {
+            "title": "Match Play",
+            "name": "Joseph Gunn",
+            "phone": "(818) 640-9446",
+            "email": "joegunn@aol.com",
+            "img": "./images/gallery/joe_gunn.jpeg"
+        },
+        {
+            "title": "Website",
+            "name": "Saran Atwal",
+            "phone": "(818) 606-8037",
+            "email": "saranatwal@hotmail.com",
+            "img": "./images/gallery/saran_atwal.jpg"
+        },
+        {
+            "title": "Assist Treasurer",
+            "name": "Richard White",
+            "phone": "(818) 846-8383",
+            "email": "jrnwhiter@aol.com",
+            "img": ""
+        },
+        {
+            "title": "Member at Large",
+            "name": "Robert Trainer",
+            "phone": "(818) 848-8616",
+            "email": "rjcdtrain@earthlink.net",
+            "img": "./images/gallery/robert_trainer.jpg"
+        },
+        {
+            "title": "Member at Large",
+            "name": "Ed Capuano",
+            "phone": "(818) 353--4674",
+            "email": "capuano58@ca.rr.com",
+            "img": ""
+        },
+    ]
+}
